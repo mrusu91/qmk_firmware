f9ff52e85853f80d0d4d5efb049734a1a2448e76
Marian Rusu <rusumarian91@gmail.com>
2017-11-05 18:40:42 +0000

    dactyl_manuform: 5x6-mrusu: reverse pins
    Signed-off-by: Marian Rusu <rusumarian91@gmail.com>

--- keyboards/dactyl_manuform/keymaps/5x6-mrusu91/config.h
+++ keyboards/dactyl_manuform/keymaps/5x6-mrusu91/config.h
@@ -44,4 +44,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION COL2ROW
 
+
+// Reverse wiring
+#undef MATRIX_COL_PINS
+#undef MATRIX_ROW_PINS
+#define MATRIX_COL_PINS { D4, C6, D7, E6, B4, B5 }
+#define MATRIX_ROW_PINS { F6, F7, B1, B3, B2, B6 }
+
 #endif
